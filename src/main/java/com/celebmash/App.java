a49e73abb21e2168548abdca2e16dfa256bf4f56
Khye Ean Low <lowkhyeean@gmail.com>
2020-04-28 19:16:55 +1000

    added snakeyml

--- src/main/java/com/celebmash/App.java
+++
@@ -1,9 +0,0 @@
-package com.celebmash;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
---
+++ src/main/java/com/celebmash/JDAConnector.java
@@ -0,0 +1,9 @@
+package com.celebmash;
+
+import net.dv8tion.jda.api.JDABuilder;
+
+public class JDAConnector {
+    public static void main(String[] args) {
+        JDABuilder builder = JDABuilder.createDefault("NzA0NjEwMjcxNjI4NjIzODcy.XqfpqQ.M2s0_JtOXjHG_uGsLSRRCpdDzxc");
+    }
+}
