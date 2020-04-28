0dead98993797036557adb99b775f41b2489e362
Khye Ean Low <lowkhyeean@gmail.com>
2020-04-28 23:01:46 +1000

    removed unused import

--- src/main/java/com/celebmash/EventListener.java
+++ src/main/java/com/celebmash/EventListener.java
@@ -9,7 +9,6 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.requests.RestAction;
 import net.dv8tion.jda.internal.utils.tuple.Pair;
 
 /**
